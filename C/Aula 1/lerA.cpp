03d3bc65c44ac0534ca72f310f8c795ae95ed2ca
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2026-02-18 21:10:19 -0300

    Delete C directory

--- C/Aula 1/lerA.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-main(){
-	char a;
-	a = 'a';
-	printf("%c", a);
-}
--- C/Aula 5/Atividade 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-float v[8];
-
-int main()
-{
-	for(int i=1;i<=8;i++)((i%2)==0)?printf("Y:")&&scanf("%f",&v[i]):printf("X:")&&scanf("%f",&v[i]);
-	for(int i=1;i<=8;i++)((i%2)==0)?printf("%f",(v[(i-1)]+v[i])):printf("\n");
-}
